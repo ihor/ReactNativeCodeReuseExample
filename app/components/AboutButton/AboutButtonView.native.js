e8dc1d52216fb405acd3ac89b2f692d91e715b67
Ihor Burlachenko <ihor.burlachenko@gmail.com>
2017-09-12 12:50:23 +0200

    Little refactoring

--- app/components/AboutButton/AboutButtonView.native.js
+++ app/components/AboutButton/AboutButtonView.native.js
@@ -4,8 +4,8 @@ import Button from 'react-native-button';
 import styles from '../../native/styles';
 
 export default props => 
-    <Button 
-        onPress={props.onClick} 
+    <Button
+        onPress={props.onClick}
         style={styles.buttonText}
         containerStyle={styles.button}>
         About
--- app/components/HelpButton/index.js
+++ app/components/HelpButton/index.js
@@ -10,7 +10,7 @@ class HelpButton extends React.Component {
     render() {
         return (
             <HelpButtonContainer { ...this.props }/>
-        )
+        );
     }
 }
 
